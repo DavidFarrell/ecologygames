fdcf1e84d59f7191f207cd26713811b4378f0bab
DavidFarrell <davidfarrell81+github@gmail.com>
2024-12-01 10:55:40 +0000

    hugo-new.sh now uses the archetypes/blog.md properly

--- hugo-new.sh
+++ hugo-new.sh
@@ -9,34 +9,16 @@ fi
 # Define variables
 POST_NAME=$1
 DATE=$(date +"%Y%m%d")
+# Use the same path Hugo generates, avoiding hardcoding 'en'
 POST_FOLDER="content/en/blog/${DATE}_${POST_NAME// /_}"
-POST_FILE="${POST_FOLDER}/index.md"
 HEADLINE_IMAGE="content/en/blog/hugo-theme-walden/headline.jpg"
 THUMBNAIL_IMAGE="content/en/blog/hugo-theme-walden/thumbnail.jpg"
 
-# Create the folder and file
-mkdir -p "$POST_FOLDER"
-
-# Generate ISO 8601-compliant date with colon in the timezone
-DATE_ISO=$(date +"%Y-%m-%dT%H:%M:%S%z" | sed 's/\([+-][0-9]\{2\}\)\([0-9]\{2\}\)$/\1:\2/')
-
-# Create the index.md file with Hugo archetype content
-cat <<EOF > "$POST_FILE"
-+++
-title = '${POST_NAME//_/ }'
-date = ${DATE_ISO}
-draft = false
-categories = 'Undefined'
-tags = ['Undefined']
-series = 'Undefined'
-[params]
-  author = 'Undefined'
-+++
-
-<!--more-->
-EOF
+# Create the blog post using the archetype
+hugo new "blog/${DATE}_${POST_NAME// /_}/index.md" --contentDir "content/en"
 
 # Copy the images
+# Use the actual folder created by Hugo
 cp "$HEADLINE_IMAGE" "$POST_FOLDER/headline.jpg"
 cp "$THUMBNAIL_IMAGE" "$POST_FOLDER/thumbnail.jpg"
 
